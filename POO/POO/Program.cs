048e3e18514268106d22df327b6d203e88b0931d
Andrea Davila <armando.barragan@epicenter.com.co>
2023-04-10 22:06:10 +0800

    Finish Date Class

---
+++ POO/POO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2022,11, 31));
+	Console.WriteLine(new Date(2022, 12, 31));
+
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+
+}
